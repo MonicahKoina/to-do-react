483c30e9e2c224130054b62a6f9c7ed9aad8c30f
Monicah Koina <monicahkoina1@gmail.com>
2024-10-22 22:35:22 +0300

    add and delete tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
+import Todo from './Todo'
 
 function App() {
 
 
   return (
-    <>
-    <div>
-      <h1>hi</h1>
-    </div>
-    </>
+
+    <Todo/>
+
   )
 }
 
---
+++ src/Todo.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+function Todo() {
+    const [tasks, setTasks]=useState([]);
+    const [newTask, setNewTask]=useState("");
+
+    function handleInputChange(event){
+        setNewTask(event.target.value);
+
+    }
+    function addTask(){
+        if(newTask.trim() !== ""){
+            setTasks(tasks => [...tasks, newTask]);
+        setNewTask("");
+        }
+    }
+    function deleteTask(index){
+        const updatedTasks = tasks.filter((_, i) =>i !== index);
+        setTasks(updatedTasks);
+
+    }
+  return (
+    <div>
+    <div>
+        <h1>To Do List</h1>
+        <div>
+            <input 
+            type="text"
+            placeholder='Enter a task'
+            value={newTask}
+            onChange={handleInputChange}
+             />
+             <button 
+             onClick={addTask}>
+                Add</button>
+        </div>
+    </div>
+    <ol>
+        {tasks.map((task, index) => (
+            <li key={index}>
+                <span>{tasks}</span>
+                <button
+                onClick={()=>{
+                    deleteTask(index)
+                }}>
+                    Delete
+                </button>
+            </li>
+        ))}
+    </ol>
+    </div>
+  )
+}
+
+export default Todo
